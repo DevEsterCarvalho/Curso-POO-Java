9e99c0f48a3f8b673094cdeff4683bec4fc4affd
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-06-02 18:54:26 -0300

    alterando para método construtor

--- Aula04/src/main/java/com/mycompany/aula04/Caneta.java
+++ Aula04/src/main/java/com/mycompany/aula04/Caneta.java
@@ -11,6 +11,13 @@ package com.mycompany.aula04;
 public class Caneta {
    private String modelo;
    private float ponta;
+   private String cor;
+   private boolean tampada;
+   
+   public Caneta(){ //este é o método construtor
+       this.tampar();
+       this.cor = "Azul";
+   }
    
    public String getModelo(){
    return this.modelo; 
@@ -24,10 +31,20 @@ public class Caneta {
    public void setPonta(float p){
        this.ponta = p;
    }
+   
+   public void tampar (){
+       this.tampada = true;
+   }
+   
+   public void destampar (){
+       this.tampada = false;
+   }
+   
    public void status(){
        System.out.println("Sobre a caneta:");
-       System.out.println("Modelo: " + this.getModelo);
-       System.out.println("Ponta: " + this.getPonta);
-        
+       System.out.println("Modelo: " + this.getModelo());
+       System.out.println("Ponta: " + this.getPonta());
+       System.out.println("Cor: " + this.cor);
+       System.out.println("Tampada: " + this.tampada);
     }
    }
